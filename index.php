5bb2817b5d85f36c5b842e02abeabe9fa67b17e9
Ola Golek <olagolek@poczta.fm>
2025-10-08 08:37:16 +0200

    js,html,php sepearately configured

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "<h1>Hejka kochani! 💪</h1>";
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+// var_dump($path);
+
+
+switch ($path) {
+    case 'dashboard':
+        include 'public/views/dashboard.html';
+        break;
+    case 'login':
+        include 'public/views/login.html';
+        break;
+    default:
+        include 'public/views/404.html';
+        break;
+} 
+?>
---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+const header = document.querySelector('#title');
+console.log(header);
+
+header.addEventListener('click', () => {
+    header.style.color = 'green';
+})
